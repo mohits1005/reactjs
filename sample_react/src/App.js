5620baa79cdd2f86fbb6790cf7d17345c3a3c798
mohit sharma <mohit.shar1005@gmail.com>
2018-12-17 14:41:13 +0530

    starter project setup

--- sample_react/src/App.js
+++ sample_react/src/App.js
@@ -3,18 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header>
-          Heisenberg
-        </header>
-        <p>
-          This is a sample application.
-        </p>
-      </div>
-    );
-  }
+    
+    render() {
+
+        const name = 'Josh Perez';
+        const element = <h1>Hello, {name}</h1>;
+
+        
+        return ( 
+            <div className = "App" >
+                <header >
+                    Heisenberg 
+                </header> 
+                <p>
+                    This is a sample application. 
+                </p> 
+                <div id = "root" >
+                </div> 
+            </div>
+        );
+    }
+    
 }
 
 export default App;
