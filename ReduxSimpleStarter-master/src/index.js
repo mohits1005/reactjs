b6e7c307388135176741bcdfe7706432af9e050e
mohit sharma <mohit.shar1005@gmail.com>
2018-12-17 23:18:01 +0530

    rendering component into DOM

--- ReduxSimpleStarter-master/src/index.js
+++ ReduxSimpleStarter-master/src/index.js
@@ -1,15 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-
-import App from './components/app';
-import reducers from './reducers';
-
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+import ReactDom from 'react-dom';
+const App = () => {
+    return <div>Hi!</div>;
+}
+ReactDom.render(<App />, document.querySelector('.container'));
