98f4b1233c6a5f67545d39ef408eeca97d4960eb
mohit sharma <mohit.shar1005@gmail.com>
2018-12-19 12:56:45 +0530

    handling user event

--- ReduxSimpleStarter-master/src/components/search_bar.js
+++ ReduxSimpleStarter-master/src/components/search_bar.js
@@ -4,7 +4,11 @@ import React, {Component} from 'react';
 // };
 class Searchbar extends Component{
     render(){
-        return <input />;
+        return <input onChange={this.onInputChange} />;
+    }
+    onInputChange(event)
+    {
+        console.log(event.target.value);
     }
 }
 export default Searchbar;
