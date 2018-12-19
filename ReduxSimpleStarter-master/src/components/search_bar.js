46d874140c237f07c7da12d5887d15bf27c4fd65
mohit sharma <mohit.shar1005@gmail.com>
2018-12-19 11:45:43 +0530

    create and render component

---
+++ ReduxSimpleStarter-master/src/components/search_bar.js
@@ -0,0 +1,5 @@
+import React from 'react';
+const Searchbar = () => {
+    return <input />
+};
+export default Searchbar;
--- ReduxSimpleStarter-master/src/index.js
+++ ReduxSimpleStarter-master/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import SearchBar from './components/search_bar';
 const API_KEY = 'AIzaSyDdXsYvX0OIKblBypD5IZMlO5OtIk1v8sQ';
 const App = () => {
-    return <div>Hi!</div>;
+    return (<div><SearchBar /></div>);
 }
 ReactDom.render(<App />, document.querySelector('.container'));
