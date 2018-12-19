a041141726f1f6bec13ef768826ee8e2c9112dce
mohit sharma <mohit.shar1005@gmail.com>
2018-12-19 21:00:54 +0530

    youtube search

--- ReduxSimpleStarter-master/src/index.js
+++ ReduxSimpleStarter-master/src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 const API_KEY = 'AIzaSyDdXsYvX0OIKblBypD5IZMlO5OtIk1v8sQ';
+YTSearch({key:API_KEY, term: 'surfboards'}, function(data){
+    console.log(data);
+})
 const App = () => {
     return (<div><SearchBar /></div>);
 }
