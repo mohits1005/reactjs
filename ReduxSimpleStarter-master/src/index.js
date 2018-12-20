0ac7783ba60c3cfc8e94301180b81e2ec60b0316
mohit sharma <mohit.shar1005@gmail.com>
2018-12-20 15:24:43 +0530

    communicating between child and parent component

--- ReduxSimpleStarter-master/src/index.js
+++ ReduxSimpleStarter-master/src/index.js
@@ -11,18 +11,24 @@ const API_KEY = 'AIzaSyDdXsYvX0OIKblBypD5IZMlO5OtIk1v8sQ';
 class App extends Component{
     constructor(props){
         super(props);
-        this.state = {videos:[]};
+        this.state = {
+            videos:[],
+            selectedVideo: null
+        };
         YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
             // console.log(data);
-            this.setState({videos});
+            this.setState({
+                videos,
+                selectedVideo: videos[0]
+            });
         })
     }
     render(){
         return (
         <div>
             <SearchBar />
-            <VideoDetail video={this.state.videos[0]}/>
-            <VideoList videos={this.state.videos}/>
+            <VideoDetail video={this.state.selectedVideo}/>
+            <VideoList videos={this.state.videos} onVideoSelect={selectedVideo => this.setState({selectedVideo})}/>
         </div>
         );
     }
