34bb6cdf56dae497f46f2dfd22e2bc94983d9c24
mohit sharma <mohit.shar1005@gmail.com>
2018-12-20 10:11:10 +0530

    Refactoring functional components to class based components

--- ReduxSimpleStarter-master/src/index.js
+++ ReduxSimpleStarter-master/src/index.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDom from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 const API_KEY = 'AIzaSyDdXsYvX0OIKblBypD5IZMlO5OtIk1v8sQ';
-YTSearch({key:API_KEY, term: 'surfboards'}, function(data){
-    console.log(data);
-})
-const App = () => {
-    return (<div><SearchBar /></div>);
+// const App = () => {
+//     return (<div><SearchBar /></div>);
+// }
+class App extends Component{
+    constructor(props){
+        super(props);
+        this.state = {videos:[]};
+        YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+            // console.log(data);
+            this.setState({videos});
+        })
+    }
+    render(){
+        return (<div><SearchBar /></div>);
+    }
 }
 ReactDom.render(<App />, document.querySelector('.container'));
