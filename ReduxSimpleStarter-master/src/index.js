22911fe7d412626039d7d6306ca7bfc3903fb1c7
mohit sharma <mohit.shar1005@gmail.com>
2018-12-20 12:56:00 +0530

    wiring up video detail component

--- ReduxSimpleStarter-master/src/index.js
+++ ReduxSimpleStarter-master/src/index.js
@@ -3,6 +3,7 @@ import ReactDom from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyDdXsYvX0OIKblBypD5IZMlO5OtIk1v8sQ';
 // const App = () => {
 //     return (<div><SearchBar /></div>);
@@ -20,6 +21,7 @@ class App extends Component{
         return (
         <div>
             <SearchBar />
+            <VideoDetail video={this.state.videos[0]}/>
             <VideoList videos={this.state.videos}/>
         </div>
         );
