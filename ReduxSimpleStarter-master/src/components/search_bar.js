7e532f3c40fb66b2135b9c2ffa8786c8dfa245c6
mohit sharma <mohit.shar1005@gmail.com>
2018-12-20 12:33:07 +0530

    wiring up video list items

--- ReduxSimpleStarter-master/src/components/search_bar.js
+++ ReduxSimpleStarter-master/src/components/search_bar.js
@@ -5,7 +5,7 @@ import React, {Component} from 'react';
 class Searchbar extends Component{
     constructor(props){
         super(props);
-        this.state = {term:'Starting Value'};
+        this.state = {term:''};
     }
     render(){
         return(
--- ReduxSimpleStarter-master/src/components/video_list_item.js
+++ ReduxSimpleStarter-master/src/components/video_list_item.js
@@ -1,5 +1,21 @@
 import React from 'react';
-const VideoListItem = (props) => {
-    return <li>Video</li>;
+const VideoListItem = ({video}) => {
+    // const video = props.video;
+    // console.log(video);
+    const imageUrl = video.snippet.thumbnails.default.url;
+    return (
+        <li className="list-group-item">
+            <div className="video-list media"> 
+                <div className="media-left">
+                    <img className="media-object" src={imageUrl}></img>
+                </div>
+                <div className="media-body">
+                    <div className="media-heading">
+                        {video.snippet.title}
+                    </div>
+                </div>
+            </div>
+        </li>  
+    );
 };
 export default VideoListItem;
