cc500474a7565e52c759a5fd3da20dec1d0e430e
mohit sharma <mohit.shar1005@gmail.com>
2018-12-20 10:24:18 +0530

    sending props from parent component to child component

--- ReduxSimpleStarter-master/src/index.js
+++ ReduxSimpleStarter-master/src/index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import ReactDom from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyDdXsYvX0OIKblBypD5IZMlO5OtIk1v8sQ';
 // const App = () => {
 //     return (<div><SearchBar /></div>);
@@ -16,7 +17,12 @@ class App extends Component{
         })
     }
     render(){
-        return (<div><SearchBar /></div>);
+        return (
+        <div>
+            <SearchBar />
+            <VideoList videos={this.state.videos}/>
+        </div>
+        );
     }
 }
 ReactDom.render(<App />, document.querySelector('.container'));
