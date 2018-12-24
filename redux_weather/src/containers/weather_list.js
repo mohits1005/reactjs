20a7df4d4c6c71479a3ea6fa29904961a1cea1e9
mohit sharma <mohit.shar1005@gmail.com>
2018-12-24 21:39:28 +0530

    Adding Google Maps Component

--- redux_weather/src/containers/weather_list.js
+++ redux_weather/src/containers/weather_list.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 export class WeatherList extends Component {
     renderWeather(cityData){
         const name = cityData.city.name;
         const temps = cityData.list.map(weather => weather.main.temp);
         const pressures = cityData.list.map(weather => weather.main.pressure);
         const humidities = cityData.list.map(weather => weather.main.humidity);
-        // console.log(temps);
+        const {lon,lat} = cityData.city.coord;
+        console.log(lon+" "+ lat);
         return (
             <tr key={name}>
-                <td>{name}</td>
+                <td><GoogleMap lon={lon} lat={lat} /></td>
                 <td><Chart data={temps} color="blue" /></td>
                 <td><Chart data={pressures} color="green" /></td>
                 <td><Chart data={humidities} color="orange" /></td>
