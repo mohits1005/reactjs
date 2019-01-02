fc9730bfac922747e870a804d6691e485799ee45
mohit sharma <mohit.shar1005@gmail.com>
2019-01-02 10:53:59 +0530

    refactoring code by adding renderField method to component

--- redux_form_practise2/src/components/user_form.js
+++ redux_form_practise2/src/components/user_form.js
@@ -1,5 +1,14 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
+const renderField = ({ input, label, type, meta: { touched, error, warning } }) => (
+    <div>
+        <label className="control-label">{label}</label>
+        <div>
+            <input {...input} placeholder={label} type={type} className="form-control" />
+            {touched && ((error && <span className="text-danger">{error}</span>) || (warning && <span>{warning}</span>))}
+        </div>
+    </div>
+)
 class UserForm extends Component{
     render() {
         const { handleSubmit } = this.props;
@@ -7,15 +16,11 @@ class UserForm extends Component{
             <form onSubmit={handleSubmit}>
                 <div className="form-group">
                     <div>
-                        <label className="control-label">First Name</label>
-                        <Field name="firstName"
-                            component={firstName => <input type="text" className="form-control" {...firstName.input} placeholder="First Name" />}
+                        <Field name="firstName" component={renderField} label="First Name"
                         />
                     </div>
                     <div>
-                        <label className="control-label">Last Name</label>
-                        <Field name="lastName"
-                            component={lastName => <input type="text" className="form-control" {...lastName.input} placeholder="Last Name" />}
+                        <Field name="lastName" component={renderField} label="First Name"
                         />
                     </div>
                     <button type="submit" className="btn btn-primary">Submit</button>
