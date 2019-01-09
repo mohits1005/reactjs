88b63c8577e14c3578b19fc641314119131daf07
mohit sharma <mohit.shar1005@gmail.com>
2019-01-09 15:42:44 +0530

    state and lifecycle example

--- react docs/hello_world/src/App.js
+++ react docs/hello_world/src/App.js
@@ -1,49 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-function formatDate(date) {
-  return date.toLocaleDateString();
-}
-const UserInfo = (props) => {
-  return (
-    <div className="UserInfo">
-      <img
-        className="Avatar"
-        src={props.user.avatarUrl}
-        alt={props.user.name}
-      />
-      <div className="UserInfo-name">
-        {props.user.name}
-      </div>
-    </div>
-  )
-}
-const Comment = (props) => {
-  return (
-    <div className="Comment">
-      <UserInfo user={props.author}/>
-      <div className="Comment-text">{props.text}</div>
-      <div className="Comment-date">
-        {formatDate(props.date)}
-      </div>
-    </div>
-  )
-};
-const comment = {
-  date: new Date(),
-  text: 'I hope you enjoy learning React!',
-  author: {
-    name: 'Hello Kitty',
-    avatarUrl: 'https://placekitten.com/g/64/64',
-  },
-};
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.setState({
+        date: new Date()
+      }),
+      1000
+    );
+  }
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
   render() {
-    return <Comment
-      date={comment.date}
-      text={comment.text}
-      author={comment.author}
-    />;
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
   }
 }
 export default App;
--- react docs/readme.md
+++ react docs/readme.md
@@ -1,3 +1,8 @@
 React - Javascript library for building user interfaces
 React elements are immutable. Once you create an element, you can’t change its children or attributes.
 When React sees an element representing a user-defined component, it passes JSX attributes to this component as a single object.
+We want to set up a timer whenever the Clock is rendered to the DOM for the first time. This is called “mounting” in React.
+We also want to clear that timer whenever the DOM produced by the Clock is removed. This is called “unmounting” in React.
+We can declare special methods on the component class to run some code when a component mounts and unmounts
+These methods are called “lifecycle methods”.
+The componentDidMount() method runs after the component output has been rendered to the DOM. 
