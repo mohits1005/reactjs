52b5f5101ccf0d8675d991e382e5c830351104ab
mohit sharma <mohit.shar1005@gmail.com>
2019-01-09 13:22:20 +0530

    Splitting component into smaller components and using props

--- react docs/hello_world/src/App.js
+++ react docs/hello_world/src/App.js
@@ -1,15 +1,44 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-// const App = (props) => {
-//   return <h1>Hello, {props.name}</h1>;
-// }
-const Welcome = (props) => {
-  return <h1>Hello, {props.name}</h1>;
+function formatDate(date) {
+  return date.toLocaleDateString();
 }
+const Comment = (props) => {
+  return (
+    <div className="Comment">
+      <div className="UserInfo">
+        <img
+          className="Avatar"
+          src={props.author.avatarUrl}
+          alt={props.author.name}
+        />
+        <div className="UserInfo-name">
+          {props.author.name}
+        </div>
+      </div>
+      <div className="Comment-text">{props.text}</div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
+    </div>
+  )
+};
+const comment = {
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl: 'https://placekitten.com/g/64/64',
+  },
+};
 class App extends React.Component {
   render() {
-    return <Welcome name='Mohit' />;
+    return <Comment
+      date={comment.date}
+      text={comment.text}
+      author={comment.author}
+    />;
   }
 }
 export default App;
