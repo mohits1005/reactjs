c03b596997fed28c88b2cb431dbb5021e844cf6d
mohit sharma <mohit.shar1005@gmail.com>
2019-01-09 12:46:10 +0530

    Class component

--- react docs/hello_world/src/App.js
+++ react docs/hello_world/src/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-const App = (props) => {
-  return <h1>Hello, {props.name}</h1>;
+// const App = (props) => {
+//   return <h1>Hello, {props.name}</h1>;
+// }
+class App extends React.Component {
+  render() {
+    return <h1>Hello, {this.props.name}</h1>;
+  }
 }
 export default App;
