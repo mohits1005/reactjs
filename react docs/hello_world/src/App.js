de75e0caeded6c0e016cb68aeae8aad7bd5165e8
mohit sharma <mohit.shar1005@gmail.com>
2019-01-09 13:28:26 +0530

    multiple components example

--- react docs/hello_world/src/App.js
+++ react docs/hello_world/src/App.js
@@ -4,19 +4,24 @@ import './App.css';
 function formatDate(date) {
   return date.toLocaleDateString();
 }
+const UserInfo = (props) => {
+  return (
+    <div className="UserInfo">
+      <img
+        className="Avatar"
+        src={props.user.avatarUrl}
+        alt={props.user.name}
+      />
+      <div className="UserInfo-name">
+        {props.user.name}
+      </div>
+    </div>
+  )
+}
 const Comment = (props) => {
   return (
     <div className="Comment">
-      <div className="UserInfo">
-        <img
-          className="Avatar"
-          src={props.author.avatarUrl}
-          alt={props.author.name}
-        />
-        <div className="UserInfo-name">
-          {props.author.name}
-        </div>
-      </div>
+      <UserInfo user={props.author}/>
       <div className="Comment-text">{props.text}</div>
       <div className="Comment-date">
         {formatDate(props.date)}
