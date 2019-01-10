f82cbc6863eb7766e4f8a50b090e7fc872d00ccb
mohit sharma <mohit.shar1005@gmail.com>
2019-01-10 12:02:06 +0530

    simple sign in sign out app using conditional rendering

--- react docs/hello_world/src/App.js
+++ react docs/hello_world/src/App.js
@@ -1,17 +1,38 @@
 import React, { Component } from 'react';
 import './App.css';
-import Clock from './components/clock';
-const FormattedDate = (props) => {
-  return (
-    <h2>It is {props.date.toLocaleTimeString()}.</h2>
-  );
+const UserGreeting = () => {
+  return <h1>Welcome back!</h1>;
+}
+const GuestGreeting = () => {
+  return <h1>Please sign up.</h1>;
+}
+const GreetingMessage = (props) => {
+  const isLoggedIn = props.isLoggedIn;
+    if (isLoggedIn) {
+      return <UserGreeting />;
+    }
+    return <GuestGreeting />;
 }
 class App extends React.Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = { isLoggedIn: false };
+  }
+  handleClick = () => {
+    const isLoggedIn = this.state.isLoggedIn;
+    if (isLoggedIn) {
+      this.setState({ isLoggedIn: false });
+    }
+    else
+      this.setState({ isLoggedIn: true });
+  }
   render() {
     return (
       <div>
-        <Clock />
+        <GreetingMessage isLoggedIn={this.state.isLoggedIn}/>
+        <button onClick={this.handleClick} >
+          {this.state.isLoggedIn ? 'Sign Out' : 'Sign In'}
+        </button>
       </div>
     );
   }
