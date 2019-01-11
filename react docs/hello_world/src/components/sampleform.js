b70c49489acf5e3848b70b6071f84f108b6a78f8
mohit sharma <mohit.shar1005@gmail.com>
2019-01-11 10:48:00 +0530

    React form fields example

--- react docs/hello_world/src/components/sampleform.js
+++ react docs/hello_world/src/components/sampleform.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 export default class SampleForm extends React.Component {
     constructor(props){
         super(props);
-        this.state = {email: '', pswd: ''};
+        this.state = { email: '', pswd: '', msg: 'grapefruit', essay: '', isGoing: true};
     }
     handleInputChange = (event) => {
         const target = event.target;
-        const value = target.value;
+        // const value = target.value;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
         const name = target.name;
         this.setState({
             [name]: value
@@ -14,7 +15,10 @@ export default class SampleForm extends React.Component {
     }
     handleSubmit = (event) => {
         event.preventDefault();
-        console.log(this.state.email+' '+this.state.pswd);
+        console.log(this.state.email + ' ' + this.state.pswd + ' ' + this.state.msg+' '+this.state.essay+' '+this.state.isGoing);
+    }
+    handleFileChange(selectorFiles) {
+        console.log(selectorFiles);
     }
     render() {
         return (
@@ -28,6 +32,35 @@ export default class SampleForm extends React.Component {
                 <label>
                     Password:
                     <input type="password" name="pswd" value={this.state.pswd} onChange={this.handleInputChange}/>
+                </label>
+                <br />
+                <label>
+                    Fruit:
+                    <select name="msg" value={this.state.msg} onChange={this.handleInputChange}>
+                        <option value="grapefruit">Grapefruit</option>
+                        <option value="lime">Lime</option>
+                        <option value="coconut">Coconut</option>
+                        <option value="mango">Mango</option>
+                    </select>
+                </label>
+                <br />
+                <label>
+                    Essay:
+                    <textarea name="essay" value={this.state.essay} onChange={this.handleInputChange} />
+                </label>
+                <br />
+                <label>
+                    Is going:
+                    <input
+                        name="isGoing"
+                        type="checkbox"
+                        checked={this.state.isGoing}
+                        onChange={this.handleInputChange} />
+                </label>
+                <br />
+                <label>
+                    Upload file:
+                    <input type="file" onChange={(e) => this.handleFileChange(e.target.files)} />
                 </label>
                 <br />
                 <input type="submit" value="Submit" />
