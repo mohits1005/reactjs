fb4d414cd16a69fc86d6f15256003b83fdd084c8
mohit sharma <mohit.shar1005@gmail.com>
2019-01-16 12:52:06 +0530

    Initial setup - feed, feed element

---
+++ forum_app/src/components/feed_element.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+export default class FeedElement extends Component {
+    render() {
+        return (
+            <div>
+                Hello there!
+            </div>
+        )
+    }
+}
---
+++ forum_app/src/components/feed_list.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+class FeedList extends Component {
+    constructor(props) {
+        super(props);
+        this.renderPost = this.renderPost.bind(this);
+    }
+    renderPost(postData){
+        const id = postData.id;
+        const category = postData.category;
+        const title = postData.title;
+        const content = postData.content;
+        return (
+            <div key={id} className="post-block">
+                <h3>{title}</h3>
+                <div>
+                    {category}
+                </div>
+                <div>
+                    {content}
+                </div>
+            </div>
+        );
+    }
+    render() {
+        const posts = this.props.posts;
+        return (
+            <div className="row">
+                <div className="col-xs-12 col-sm-10 col-md-8 center-container">
+                    { posts.map(this.renderPost) }
+                </div>
+            </div>
+        );
+    }
+}
+
+export default FeedList;
