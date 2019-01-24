9758d70ccdc02240de00b69d454742214dffac31
mohit sharma <mohit.shar1005@gmail.com>
2019-01-25 00:53:19 +0530

    toggle add comment option - forum app

--- forum_app/src/components/comments_list.js
+++ forum_app/src/components/comments_list.js
@@ -2,8 +2,26 @@ import React, { Component } from 'react';
 class CommentsList extends Component {
     constructor(props) {
         super(props);
+        this.state = { value: '', isVisible: false };
         this.renderComment = this.renderComment.bind(this);
         this.renderReplies = this.renderReplies.bind(this);
+        this.onFormSubmit = this.onFormSubmit.bind(this);
+        this.onInputChange = this.onInputChange.bind(this);
+        this.showAddComment = this.showAddComment.bind(this);
+    }
+    onInputChange(event) {
+        this.setState({ value: event.target.value });
+    }
+    onFormSubmit(event) {
+        event.preventDefault();
+        const comments = this.props.comments;
+        const id = comments.length + 1;
+        const userId = 10;
+        const userName = 'Anonymous';
+        const text = this.state.value;
+        const replies = [];
+        const comment = { id, userId, userName, text, replies };
+        this.props.onAddcomment(comment);
     }
     renderReplies(repliesData){
         const { id, text, userName } = repliesData;
@@ -44,21 +62,34 @@ class CommentsList extends Component {
             </div>
         );
     }
+    showAddComment(){
+        this.setState({isVisible:true});
+    }
     render() {
         const comments = this.props.comments;
         const comments_count = comments.length;
         const comments_string = comments_count > 0 ? 'There are '+comments_count + ' Comments ':'';
+        const addinput =
+            <div className="input-wrap">
+                <form className="add-input-form" onSubmit={this.onFormSubmit}>
+                    <input className="add-comment-input form-control" type="text" placeholder="Add Comment..." onChange={this.onInputChange} value={this.state.value} />
+                    <button type="submit" className="add-comment-btn">POST</button>
+                </form>
+            </div>;
         return (
-            <div className="row">
-                <div className="col-xs-12 col-sm-10 col-md-8 comments-container">
-                    <div className="comments-count">
-                        {comments_string}
-                        <span className="add-comment">
-                            ADD YOURS
-                        </span>
+            <div>
+                <div className="row">
+                    <div className="col-xs-12 col-sm-10 col-md-8 comments-container">
+                        <div className="comments-count">
+                            {comments_string}
+                            <span className="add-comment" onClick={this.showAddComment}>
+                                ADD YOURS
+                            </span>
+                        </div>
+                        {comments.map(this.renderComment)}
                     </div>
-                    {comments.map(this.renderComment)}
                 </div>
+                {this.state.isVisible && addinput}
             </div>
         );
     }
--- forum_app/src/components/post_detail.js
+++ forum_app/src/components/post_detail.js
@@ -5,30 +5,18 @@ import COMMENTS from '../comments';
 class PostDetail extends Component {
     constructor(props){
         super(props);
-        this.state = { value: '', comments: COMMENTS };
-        this.onFormSubmit = this.onFormSubmit.bind(this);
-        this.onInputChange = this.onInputChange.bind(this);
+        this.state = {comments: COMMENTS};
+        this.onAddcomment = this.onAddcomment.bind(this);
     }
-    onInputChange(event) {
-        this.setState({ value: event.target.value });
+    onAddcomment(comment){
+        const comments = this.state.comments;
+        comments.push(comment);
+        this.setState({ comments });
     }
     changeFilterText(value) {
-        console.log('Search text: '+value);
-    }
-    onFormSubmit(event){
-        event.preventDefault();
-        const comments = this.state.comments;
-        const id = COMMENTS.length+1;
-        const userId = 10;
-        const userName = 'Anonymous';
-        const text = this.state.value;
-        const replies = [];
-        const comment = {id, userId, userName, text, replies};
-        comments.push(comment);
-        this.setState({comments});
+        return;
     }
     render() {
-        // const { id } = this.props.match.params;
         const { postData } = this.props.location.state;
         return (
             <div>
@@ -44,13 +32,7 @@ class PostDetail extends Component {
                         {postData.content}
                     </div>
                     <hr />
-                    <CommentsList comments={this.state.comments}/>
-                    <div className="input-wrap">
-                        <form className="add-input-form" onSubmit={this.onFormSubmit}>
-                            <input className="add-comment-input form-control" type="text" placeholder="Add Comment..." onChange={this.onInputChange}/>
-                            <button type="submit" className="add-comment-btn">POST</button>
-                        </form>
-                    </div>
+                    <CommentsList comments={this.state.comments} onAddcomment={this.onAddcomment}/>
                 </div>
             </div>
         );
