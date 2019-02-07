f5d9b33a8c985ee601356bdc62da530959c4915c
mohit sharma <mohit.shar1005@gmail.com>
2019-02-07 23:31:14 +0530

    edit comment feature fix - forum app

--- forum_app/src/components/comment.js
+++ forum_app/src/components/comment.js
@@ -3,19 +3,12 @@ import Reply from './reply';
 class Comment extends Component {
     constructor(props) {
         super(props);
-        this.state = { editCommentId: -1, editCommentValue: '' }
+        this.state = { editCommentId: -1 }
         this.toggleEditComment = this.toggleEditComment.bind(this);
         this.saveEditComment = this.saveEditComment.bind(this);
-        this.onInputChange = this.onInputChange.bind(this);
     }
-    toggleEditComment(id, value) {
-        if (value) {
-            this.setState({ editCommentId: id, editCommentValue: value});
-        }
-        else
-        {
-            this.setState({ editCommentId: -1 });
-        }
+    toggleEditComment(id) {
+        this.setState({ editCommentId: id});
     }
     renderReplies(replyData, commentId) {
         const { id } = replyData;
@@ -25,29 +18,25 @@ class Comment extends Component {
             </div>
         );
     }
-    onInputChange(event) {
-        this.setState({ editCommentValue: event.target.value });
-    }
-    saveEditComment() {
+    saveEditComment(val) {
         const id = this.state.editCommentId;
-        const val = this.state.editCommentValue;
         this.props.onEditcomment({ id, val });
         this.toggleEditComment(-1);
     }
     render() {
         const { id, userId, text, userName, replies } = this.props.commentData;
         const editCommentId= this.state.editCommentId;
-        const editCommentValue = this.state.editCommentValue;
         const editCommentText =
             <div className="edit-comment-wrap" onClick={() => this.toggleEditComment(id, text)} >
                 <div className="edit-comment-text">Edit</div>
             </div>;
-        const editCommentInput =
-            <div className="save-comment-wrap">
-                <input type="text" className="form-control" value={editCommentValue} onChange={this.onInputChange} />
-                <div className="save-comment-text" onClick={this.saveEditComment}>Save</div>
-                <div className="cancel-comment-text" onClick={() => this.toggleEditComment(-1)}>Cancel</div>
-            </div>;
+        let showEditCommentInput;
+        if (userId === 10 && id === editCommentId)
+        {
+            showEditCommentInput = 
+                <EditCommentInput text={text} saveEditComment={this.saveEditComment} toggleEditComment={this.toggleEditComment}>
+                </EditCommentInput>;
+        }
         return (
             <div>
                 <div className="comment-wrap">
@@ -56,12 +45,13 @@ class Comment extends Component {
                     </div>
                     <div>
                         {id !== editCommentId && text }
-                        {userId === 10 && id === editCommentId && editCommentInput }
+                        {showEditCommentInput}
                     </div>
                     <div className="time">
                         Posted on Jan 18, 2019
                     </div>
                     {userId === 10 && id !== editCommentId && editCommentText }
+                    {this.displayEditComment}
                 </div>
                 <div className="replies-wrap">
                     {replies.map((reply) => this.renderReplies(reply, id))}
@@ -69,5 +59,27 @@ class Comment extends Component {
             </div>
         );
     }
+}
+class EditCommentInput extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { editCommentValue: props.text };
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+    onInputChange(event) {
+        this.setState({ editCommentValue: event.target.value });
+    }
+    render() {
+        const editCommentValue = this.state.editCommentValue;
+        if (!this.props.isVisible)
+        return (
+            <div className="save-comment-wrap">
+                <input type="text" className="form-control" value={editCommentValue} onChange={this.onInputChange} />
+                <div className="save-comment-text" onClick={() => this.props.saveEditComment(editCommentValue)}>Save</div>
+                <div className="cancel-comment-text" onClick={() => this.props.toggleEditComment(-1)}>Cancel</div>
+            </div>
+        );
+    }
+
 }
 export default Comment;
